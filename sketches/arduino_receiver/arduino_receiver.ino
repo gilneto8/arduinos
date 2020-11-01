d49a6b694491aa4a916d10e8dbce4037b8d7c8dc
Gil Neto <gilneto8@gmail.com>
2020-11-01 15:18:43 +0000

    Updated projects

--- sketches/arduino_receiver/arduino_receiver.ino
+++ sketches/arduino_receiver/arduino_receiver.ino
@@ -1,11 +1,8 @@
-#include <nRF24L01.h>
-#include <printf.h>
+//#include <RF24Network.h>
 #include <RF24.h>
-#include <RF24_config.h>
-
 #include <SPI.h>
 
-RF24 myRadio (10, 9);
+RF24 radio (7, 8);
 struct package
 {
   int id=0;
@@ -13,28 +10,42 @@ struct package
   char  text[100] ="empty";
 };
 typedef struct package Package;
+
+/*RF24Network network(radio);      // Network uses that radio
+const uint16_t this_node = 01;    // Address of our node in Octal format ( 04,031, etc)
+const uint16_t base = 00;   // Address of the other node in Octal format
+RF24NetworkHeader header;*/
+const byte addr[5] = {'R','x','A','A','A'};
+
 Package data;
-const uint64_t pipe = 0xE6E6E6B2B2B2;
 
 void setup() {
   Serial.begin(9600);
   delay(1000);
-  myRadio.begin(); 
-  myRadio.openReadingPipe(1, pipe);
-  myRadio.startListening();
+  //SPI.begin();
+  radio.begin();
+  radio.openReadingPipe(1, addr);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.setDataRate(RF24_250KBPS);
+  radio.startListening();
+  //network.begin(/*channel*/ 110, /*node address*/ this_node);
+  delay(1000);
 }
 
 void loop() {
-  if ( myRadio.available()) 
+  //network.update();
+  //RF24NetworkHeader header_sender(base);
+  if ( radio.available()) 
   {
-    while (myRadio.available())
-    {
-      myRadio.read( &data, sizeof(data) );
+    while(radio.available()){
+      //network.read(header, &data, sizeof(data) );
+      radio.read(&data, sizeof(data) );
+      Serial.print("\nPackage:");
+      Serial.print(data.id);
+      Serial.print("\n");
+      Serial.print("Data:");
+      Serial.println(data.temperature);
+      Serial.println(data.text);
     }
-    Serial.print("\nPackage:");
-    Serial.print(data.id);
-    Serial.print("\n");
-    Serial.println(data.temperature);
-    Serial.println(data.text);
   }
 }
--- sketches/arduino_sender/arduino_sender.ino
+++ sketches/arduino_sender/arduino_sender.ino
@@ -1,8 +1,6 @@
 #include <RF24.h>
-#include <RF24Network.h>
+//#include <RF24Network.h>
 #include <SPI.h>
-
-RF24 radio (7, 8);
 
 struct package
 {
@@ -11,27 +9,45 @@ struct package
   char  text[100] = "Text to be transmitted";
 };
 typedef struct package Package;
+
+RF24 radio (7, 8);
+/*RF24Network network(radio);      // Network uses that radio
+const uint16_t this_node = 00;    // Address of our node in Octal format ( 04,031, etc)
+const uint16_t node01 = 01;   // Address of the other node in Octal format
+RF24NetworkHeader header;*/
+const byte receiver_addr[5] = {'R','x','A','A','A'};
+
 Package data;
-const uint64_t pipe = 0xE6E6E6B2B2B2;
 
 void setup()
 {
   Serial.begin(9600);
   delay(1000);
+  //SPI.begin();
   radio.begin();
-  radio.openWritingPipe(pipe);
+  radio.openWritingPipe(receiver_addr);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.setDataRate(RF24_250KBPS);
+  //network.begin(/*channel*/ 110, /*node address*/ this_node);
   delay(1000);
 }
 
 void loop()
 {
-  radio.write(&data, sizeof(data)); 
-  Serial.print("\nPackage:");
-  Serial.print(data.id);
-  Serial.print("\n");
-  Serial.println(data.temperature);
-  Serial.println(data.text);
-  data.id = data.id + 1;
-  data.temperature = data.temperature+0.1;
-  delay(1000);
+  //RF24NetworkHeader header_receiver(node01);
+  //network.write(header_receiver, &data, sizeof(data));
+  if ( radio.available()) 
+  {
+    while(radio.available()){
+      radio.write(&data, sizeof(data));
+      Serial.print("\nPackage:");
+      Serial.print(data.id);
+      Serial.print("\n");
+      Serial.println(data.temperature);
+      Serial.println(data.text);
+      data.id = data.id + 1;
+      data.temperature = data.temperature+0.1;
+      delay(1000);
+    }
+  }
 }
